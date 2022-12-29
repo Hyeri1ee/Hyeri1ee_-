faba62ba644e3a3789811c30c15875077c952c32
이혜리(Hyeri Lee) <108603070+Hyeri1ee@users.noreply.github.com>
2022-12-29 19:15:53 +0900

    Add files via upload

--- 구현중/Jframe.java
+++ 구현중/Jframe.java
@@ -1,7 +1,10 @@
 package e;
 
 import java.awt.Container;
+
 import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
 
 @SuppressWarnings("serial")
 public class Jframe extends JFrame{
@@ -10,14 +13,30 @@ public class Jframe extends JFrame{
 		this.setTitle(title);
 		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
 		this.setLayout(null);
-		init();
+		createMenu();
+		firstDefaultPage();
 		this.setSize(500,500);
 		this.setVisible(true);
 	}
-	public void init() {
+
+	public void firstDefaultPage() {
 		// TODO Auto-generated method stub
 		
 	}
+
+	public void createMenu() {
+		// TODO Auto-generated method stub
+		JMenuBar mb = new JMenuBar();
+		JMenu menu1 = new JMenu("Plan");
+		JMenu menu2 = new JMenu("Current");
+		
+		//.txt 파일에 있는 메뉴 아이템 읽어서 만드는 함수 입력 부분
+		
+		mb.add(menu1);
+		mb.add(menu2);
+		this.setJMenuBar(mb);
+	}
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		new Jframe("22/23 p");
